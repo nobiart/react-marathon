e79f13ff9b6fcdd2976fa26f66996dfe73747c19
nobiart <dmitri.besleaga@gmail.com>
2021-01-28 20:20:13 +0200

    Homework #2. Added 3 new components (Header, Layout, Footer)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import HeaderBlock from "./components/HeaderBlock";
+//import HeaderBlock from "./components/HeaderBlock";
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
+
+import Bg1 from './assets/bg2.jpg';
+import Bg2 from './assets/bg1.jpg';
 
 const App = () => {
     return (
         <>
-            <HeaderBlock
-                title="This is new title"
-                descr="This is new description!"
+            <Header
+                title="This is title"
+                descr="This is Description!"
             />
-            <HeaderBlock
-                title="This is new title"
-                hideBackground
+            <Layout
+                id="withBg1"
+                urlBg={`${Bg1}`}
+                title="This is layout #1 title"
+                descr="This is layout #1 Description!"
             />
+            <Layout
+                id="withColorBg"
+                colorBg="#ffbe55"
+                title="This is layout #2 title"
+                descr="This is layout #2 Description!"
+            />
+            <Layout
+                id="withBg2"
+                urlBg={`${Bg2}`}
+                title="This is layout #3 title"
+                descr="This is layout #3 Description!"
+            />
+            <Footer />
         </>
-    );
+    )
 }
 
 export default App;
