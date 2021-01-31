6e4af9eca14799aa366ac606f6e9ef29968846ec
nobiart <dmitri.besleaga@gmail.com>
2021-01-31 17:41:57 +0200

    Homework #3. Changes after review

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -8,7 +8,7 @@ const PokemonCard = ({name, img, id, type, values}) => {
     const [isActive, setActive] = useState(false);
 
     const handleClick = () => {
-        setActive(true);
+        setActive(!isActive);
     }
 
     return (
